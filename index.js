3925568c12fb639ad755b1b36520e32e6ea6696d
paolaCaushi <pcaushi@unitsystem.net>
2022-10-13 15:52:07 +0200

    animating the score

--- index.js
+++ index.js
@@ -21,12 +21,27 @@ form.addEventListener('submit', e=> {
     scrollTo(0,0);
     result.querySelector('span').textContent = `${score}%`;
     result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(()=>{
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        }else{
+            output++;
+        }
+    }, 10);
 });
 
 
 //window object (global object)
 
-setTimeout(() =>{
-    alert('hello, ninjas');
-}, 3000);
+let i=0;
+const timer= setInterval(() =>{
+    console.log('hello');
+    i++;
+    if(i===5){
+        clearInterval(timer);
+    }
+}, 1000);
 
